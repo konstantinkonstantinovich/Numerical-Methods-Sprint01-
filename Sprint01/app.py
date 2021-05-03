68e07eb88098850852629b62732c195f47a3b3da
Konstantin Shchegolev <konstantinshchegolev@Konstantins-MacBook-Air.local>
2021-05-03 19:23:27 +0300

    jordan-Gauss

--- Sprint01/app.py
+++ Sprint01/app.py
@@ -1,35 +1,65 @@
+import os
+import numpy as np
 
-import numpy
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 import copy
+from os.path import join, dirname, realpath
 
-UPLOAD_FOLDER = '/uploads/files/'
+UPLOAD_FOLDER = './uploads/'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+UPLOADS_PATH = join(dirname(realpath(__file__)), 'static/uploads/..')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def parse_file():
-	list_a = list()
-	list_b = list()
-	with open('file.txt', 'r') as file:
-		line = file.read()[3:].split('\n')
-		for lin in line:
-			new_list = lin.split(' ')
-			arr = list()
-			for i in new_list:
-				arr.append(int(i))
-			list_a.append(arr)
-		for i in list_a:
-			list_b.append(i[-1])
-			del i[-1]
+def Jordan_Causs(n, a, b):
+	j = 0
+	for i in a:
+		length = len(b)
+		i.append(b[j])
+		j+=1
+
+	x = np.zeros(n)
+	for i in range(n):
+	    if a[i][i] == 0.0:
+	        sys.exit('Divide by zero detected!')
+	        
+	    for j in range(n):
+	        if i != j:
+	            ratio = a[j][i]/a[i][i]
+
+	            for k in range(n+1):
+	                a[j][k] = a[j][k] - ratio * a[i][k]
+
+	# Obtaining Solution
+
+	for i in range(n):
+	    x[i] = a[i][n]/a[i][i]
+
+	return x
+
+# def parse_file(filename):
+# 	list_a = list()
+# 	list_b = list()
+# 	with open('./uploads/'+filename, 'r') as file:
+# 		line = file.read()[3:].split('\n')
+# 		for lin in line:
+# 			new_list = lin.split(' ')
+# 			arr = list()
+# 			for i in new_list:
+# 				arr.append(int(i))
+# 			list_a.append(arr)
+# 		for i in list_a:
+# 			list_b.append(i[-1])
+# 			del i[-1]
 		
 
-print(line)
-print(list_a)
-print(list_b)
+# print(line)
+# print(list_a)
+# print(list_b)
 
 
 
@@ -186,13 +216,71 @@ def Task_3():
 	return render_template('task_3.html', result=result)
 
 
+@app.route('/task_4', methods=['post', 'get'])
+def Task_4():
+	a_list = list()
+	b_list = list()
+	array = []
+	array1 = []
+	result = []
+	if request.method == 'POST':
+		a = request.form.get('A')
+		b = request.form.get('B')
+		a_list.append(a)
+		b_list.append(b)
+		array = TakeMatrix(a_list)
+		array1 = TakeB(b_list)
+		result = Jordan_Causs(3, array, array1)
+	return render_template('task_4.html', array=array, array1=array1, result=result)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/upload', methods=['post', 'get'])
 def Read_From_File():
 	pick = None
 	filename= None
+	list_a = list()
+	list_b = list()
+	result = None
 	if request.method == 'POST':
 		option = request.form.get('op')
 		file = request.files['file']
-		pick = option
-		filename = file
-	return render_template('upload_file.html', pick=pick, filename=filename)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			with open('./uploads/'+filename, 'r') as file:
+				line = file.read()[2:].split('\n')
+				for lin in line:
+					new_list = lin.split(' ')
+					arr = list()
+					for i in new_list:
+						arr.append(int(i))
+					list_a.append(arr)
+				for i in list_a:
+					list_b.append(i[-1])
+					del i[-1]
+		pick = option	
+		print(pick)		
+		if pick == '1':
+			M3 = numpy.array(list_a) 
+			v3 = numpy.array(list_b)
+			result = numpy.linalg.solve(M3, v3)
+		if pick == '2':
+			result = Gauss(list_a, list_b)
+			print(result)
+		if pick == '3':
+			result = Zeidel(list_a, list_b)
+		if pick == '4':
+			result = Jordan_Causs(3, list_a, list_b)
+	return render_template('upload_file.html', pick=pick, list_a=list_a, list_b=list_b, result=result)
+
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
--- Sprint01/file.py
+++ Sprint01/file.py
@@ -1,12 +1,12 @@
 list_a = list()
 list_b = list()
 with open('file.txt', 'r') as file:
-	line = file.read()[3:].split('\n')
+	line = file.read()[2:].split('\n')
 	for lin in line:
 		new_list = lin.split(' ')
 		arr = list()
 		for i in new_list:
-			arr.append(int(i))
+			arr.append(float(i))
 		list_a.append(arr)
 	for i in list_a:
 		list_b.append(i[-1])
@@ -16,3 +16,63 @@ with open('file.txt', 'r') as file:
 print(line)
 print(list_a)
 print(list_b)
+print('$==================*$#*=================&')
+j = 0
+for i in list_a:
+	print(i)
+	length = len(list_b)
+	i.append(list_b[j])
+	j+=1
+	print(j)
+
+
+print(list_a)
+
+
+
+
+# print('$==================*$#*=================&')
+
+
+# def SwapRows(A, B, row1, row2):
+#     A[row1], A[row2] = A[row2], A[row1]
+#     B[row1], B[row2] = B[row2], B[row1]
+
+
+# def CombineRows(A, B, row, source_row, weight):
+#     A[row] = [(a + k * weight) for a, k in zip(A[row], A[source_row])]
+#     B[row] += B[source_row] * weight
+
+
+# for row in list_a:
+# 	# print(row)
+# 	for col in range(len(row)):
+
+# 		length = len(row)
+# 		i = 0
+# 		while i < length:
+# 			if list_a[i][0] == 1:
+# 				SwapRows(list_a, list_b, i, 0)
+# 			i+=1
+
+
+
+# print(list_a)
+# print(list_b)
+# print('===========================@End swap elements@=====================ap')
+
+# for row in range(len(list_a)):
+# 	if row > 0:
+# 		for col in range(len(list_a)):
+# 			multiplier = -list_a[row][0]
+# 			f_row = list_a[0][col] * multiplier
+# 			current = list_a[row][col]
+# 			current += f_row
+# 			list_a[row][col]
+			
+# 			print('After', current)
+
+# print(list_a)
+
+
+			
